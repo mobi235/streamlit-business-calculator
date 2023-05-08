b87e296330cd2dc17567b4c8efe0f686b4f2dc2e
mobi235 <mohamad.bezreh@billie.io>
2023-05-08 09:25:19 +0200

    initialising app

---
+++ layout.py
@@ -0,0 +1,221 @@
+"""new"""
+import streamlit as st
+from PIL import Image
+import pandas as pd
+import math
+import plotly.graph_objects as go
+
+
+def coalesce(value, replacement):
+    if math.isnan(value):
+        return replacement
+    else:
+        return value
+
+
+def style_dataframe(df):
+    # Define the styles for the dataframe
+    styles = [
+        {
+            "selector": "th",
+            "props": [("background-color", "#1e1e1e"), ("color", "white")],
+        },
+        {
+            "selector": "th:not(:nth-child(2))",
+            "props": [("background-color", "#d8d8d8"), ("color", "black")],
+        },
+        {
+            "selector": "td:fist-child",
+            "props": [("background-color", "#d8d8d8"), ("color", "black")],
+        },
+        {
+            "selector": "td:nth-child(2)",
+            "props": [("background-color", "#d8d8d8"), ("color", "black")],
+        },
+        # {
+        #     "selector": "td:first-child",
+        #     "props": [("background-color", "#7r7r7r"), ("color", "#1e1e1e")],
+        # },
+        {
+            "selector": "td:not(:first-child):not(:nth-child(2))",
+            "props": [("background-color", "#f3f3f3"), ("color", "black")],
+        },
+        ##
+        # {
+        #     "selector": "td:not(:first-child):not(:nth-child(odd))",
+        #     "props": [("background-color", "#6600f5"), ("color", "white")],
+        # },
+    ]
+
+    # Create a Styler object and apply the styles
+    styled_df = df.style.set_table_styles(styles)
+
+    return styled_df
+
+
+def set_image():
+
+    image = Image.open("logo_billie.png")
+    width, height = image.size
+    new_size = (int(width / 10), int(height / 10))  # Resize to half the original size
+    resized_image = image.resize(new_size)
+
+    # Display the image with a custom height using HTML/CSS
+    st.image(resized_image, caption="", use_column_width=False)
+
+
+def get_assumptions():
+    adoption_rate = 0.5
+    adoption_rate_formatted = "{:,.0%}".format(adoption_rate)
+
+    billie_acceptance_rate = 0.9
+    billie_acceptance_rate_formatted = "{:,.0%}".format(billie_acceptance_rate)
+
+    buyers_not_accepted_for_bnpl_rate = 0.5
+    buyers_not_accepted_for_bnpl_rate_formatted = "{:,.0%}".format(
+        buyers_not_accepted_for_bnpl_rate
+    )
+
+    cart_abandon_rate = 0.3
+    cart_abandon_rate_formatted = "{:,.0%}".format(cart_abandon_rate)
+
+    increase_basket_size = 0.2
+    increase_basket_size_formatted = "{:,.0%}".format(increase_basket_size)
+
+    increased_conversion_rate = 0.15
+    increased_conversion_rate_formatted = "{:,.0%}".format(increased_conversion_rate)
+
+    assumption_df = pd.DataFrame(
+        [
+            {
+                "Assumptions": "% Billie of total B2B online payment solutions",
+                "value": adoption_rate_formatted,
+            },
+            {
+                "Assumptions": "Billie acceptance rates:",
+                "value": billie_acceptance_rate_formatted,
+            },
+            {
+                "Assumptions": "Buyers not accepted for BNPL:",
+                "value": buyers_not_accepted_for_bnpl_rate_formatted,
+            },
+            {
+                "Assumptions": "Cart abandonment rate:",
+                "value": cart_abandon_rate_formatted,
+            },
+            {
+                "Assumptions": "Increase in average basket size:",
+                "value": increase_basket_size_formatted,
+            },
+            {
+                "Assumptions": "Increase in conversion rate:",
+                "value": increased_conversion_rate_formatted,
+            },
+        ]
+    )
+    assumption_df = assumption_df.reset_index(drop=True)
+    return assumption_df
+
+
+def waterfall_fig(
+    is_high_level=False,
+    revenue=0,
+    revenue_chg_basket_size=0,
+    revenue_chg_acceptance_rate=0,
+    revenue_chg_conversion_rate=0,
+    revenue_w_billie=0,
+):
+
+    if not is_high_level:
+        fig = go.Figure(
+            go.Waterfall(
+                name="20",
+                orientation="v",
+                x=[
+                    "Current B2B Online Revenue",
+                    "Uplift potential - Higher basket size",
+                    "Uplift potential - Higher Acceptance rates",
+                    "Uplift potential - Higher conversion rates",
+                    "Subtotal",
+                ],
+                textposition="outside",
+                measure=[
+                    "relative",
+                    "relative",
+                    "relative",
+                    "relative",
+                    "total",
+                ],
+                # text = ["NET_FEE_RATE", "NET_DEFAULT_RATE", "MONTHLY_MERCHANT_REFI_COST", "INSURANCE_COST", "SCORING_COST_MARGIN", "TRANSACTION_BANKING_COST"],
+                text=[
+                    revenue,
+                    revenue_chg_basket_size,
+                    revenue_chg_acceptance_rate,  # .round(4)*100,
+                    revenue_chg_conversion_rate,
+                    revenue_w_billie,
+                ],
+                y=[
+                    revenue,
+                    revenue_chg_basket_size,
+                    revenue_chg_acceptance_rate,  # .round(4)*100,
+                    revenue_chg_conversion_rate,
+                    revenue_w_billie,
+                ],
+                base=0.0,
+                decreasing={
+                    "marker": {"color": "Maroon"}
+                },  # , "line":{"color":"red", "width":2}
+                increasing={"marker": {"color": "Teal"}},
+                totals={"marker": {"color": "deep sky blue"}}
+                # connector = {"line":{"color":"rgb(63, 63, 63)"}},
+                # connector = {"visible": False,
+                #            "line":{"color":"rgb(63, 63, 63)"}
+                #            },
+            )
+        )
+
+        fig.update_layout(title="Billie Uplift Potential", showlegend=False)
+        return fig
+    else:
+        fig = go.Figure(
+            go.Waterfall(
+                name="20",
+                orientation="v",
+                x=[
+                    "Current B2B Online Revenue",
+                    "Uplift potential - Higher basket size",
+                    "Subtotal",
+                ],
+                textposition="outside",
+                measure=[
+                    "relative",
+                    "relative",
+                    "total",
+                ],
+                # text = ["NET_FEE_RATE", "NET_DEFAULT_RATE", "MONTHLY_MERCHANT_REFI_COST", "INSURANCE_COST", "SCORING_COST_MARGIN", "TRANSACTION_BANKING_COST"],
+                text=[
+                    revenue,
+                    revenue_chg_basket_size,
+                    revenue_w_billie,
+                ],
+                y=[
+                    revenue,
+                    revenue_chg_basket_size,
+                    revenue_w_billie,
+                ],
+                base=0.0,
+                decreasing={
+                    "marker": {"color": "Maroon"}
+                },  # , "line":{"color":"red", "width":2}
+                increasing={"marker": {"color": "Teal"}},
+                totals={"marker": {"color": "deep sky blue"}}
+                # connector = {"line":{"color":"rgb(63, 63, 63)"}},
+                # connector = {"visible": False,
+                #            "line":{"color":"rgb(63, 63, 63)"}
+                #            },
+            )
+        )
+
+        fig.update_layout(title="Billie Uplift Potential", showlegend=False)
+        return fig
+    # fig.layout.yaxis.tickformat = ",.2%"
---
+++ sidebar.py
@@ -0,0 +1,179 @@
+import streamlit as st
+import pandas as pd
+
+
+def sidebar_financial():
+
+    st.sidebar.markdown("## Select Input Variables - Merchant")
+    st.sidebar.markdown("### Key Financials")
+
+    b2b_rev = st.sidebar.number_input(
+        "B2B revenues p.a. Online: (in €)", value=12000000, step=500000
+    )
+    b2b_rev_formatted = "{:,.2f}".format(b2b_rev)
+
+    gross_profit = st.sidebar.number_input(
+        "Gross profit margin: (in %)", value=20.0, step=5.0
+    )
+    gross_profit_formatted = "{:,.1%}".format(gross_profit / 100)
+    st.sidebar.markdown("### Order details - B2B Online")
+    # -- Set time by GPS or event
+
+    avg_basket = st.sidebar.number_input(
+        "Average Basket Size: (in €)", value=500, step=100
+    )
+    avg_basket_formatted = "{:,.2f}".format(avg_basket)
+    # display the float value entered by the user
+    # st.write("You entered:", b2b_rev)
+
+    st.sidebar.markdown("### BNPL Details (if applicable)")
+    # -- Set time by GPS or event
+    avg_acceptance = st.sidebar.slider(
+        "Average Acceptance Rate: (in %)",
+        value=60.0,
+        min_value=0.0,
+        max_value=100.0,
+        step=1.0,
+    )
+    avg_acceptance_formatted = "{:,.1%}".format(avg_acceptance / 100)
+
+    financial_df = pd.DataFrame(
+        [
+            {"Metric": "B2B revenues p.a. Online:", "value": b2b_rev_formatted},
+            {"Metric": "Gross profit margin:", "value": gross_profit_formatted},
+            {"Metric": "Average Basket Size:", "value": avg_basket_formatted},
+            {"Metric": "Average Acceptance Rate:", "value": avg_acceptance_formatted},
+        ]
+    )
+    financial_df = financial_df.reset_index(drop=True)
+    ######  Billie pricing ########
+    st.sidebar.markdown("### Billie pricing")
+    # -- Set time by GPS or event
+    # avg_basket = st.sidebar.slider("Average Acceptance Rate: (in %)", 0.0, 1.0, 0.05)
+    fixed_fee = st.sidebar.number_input("Fixed Fee: (in %)", value=1.69, step=0.01)
+    fixed_fee_formatted = "{:,.1%}".format(fixed_fee / 100)
+
+    # display the inputs
+    transaction_fee = st.sidebar.number_input(
+        "Transaction Fee: (in €)", value=0.5, step=0.1
+    )
+    transaction_fee_formatted = "{:,.2f}".format(transaction_fee)
+
+    blended_fee = (transaction_fee / avg_basket) * 100 + fixed_fee
+    blended_fee_formatted = "{:,.2%}".format(blended_fee / 100)
+
+    pricing_df = pd.DataFrame(
+        [
+            {"Metric": "Fixed Fee:", "value": fixed_fee_formatted},
+            {"Metric": "Transaction Fee:", "value": transaction_fee_formatted},
+            {"Metric": "Blended Fee", "value": blended_fee_formatted},
+        ]
+    )
+
+    return (
+        financial_df,
+        pricing_df,
+    )  # st.dataframe(financial_df, use_container_width=True)
+
+
+def payment_info():
+
+    ########  Payment detail input ########
+    st.sidebar.markdown("### B2B payment details - Online")
+    # -- Set time by GPS or event
+    # avg_basket = st.sidebar.slider("Average Acceptance Rate: (in %)", 0.0, 1.0, 0.05)
+    col1, col2, col3 = st.sidebar.columns(3)
+    bool_inhouse = col1.checkbox("Inhouse BNPL")
+    percent_inhouse = col2.number_input("1-Share of Vol. (%):", value=0.0, step=1.0)
+    percent_inhouse_formatted = "{:,.1%}".format(percent_inhouse / 100)
+    cost_inhouse = col3.number_input("1-Assumed Costs:", value=0.0, step=1.0)
+    cost_inhouse_formatted = "{:,.1%}".format(cost_inhouse / 100)
+    # display the inputs
+    # st.write("Boolean input:", bool_input)
+    # st.write("Percentage input:", percent_input, "%")
+    ext, ext_share, ext_cost = st.sidebar.columns(3)
+    bool_ext = ext.checkbox("External BNPL")
+    percent_ext = ext_share.number_input("2-Share of Vol. (%):", value=0.0, step=1.0)
+    percent_ext_formatted = "{:,.1%}".format(percent_ext / 100)
+    cost_ext = ext_cost.number_input("2-Assumed Costs:", value=0.0, step=1.0)
+    cost_ext_formatted = "{:,.1%}".format(cost_ext / 100)
+
+    cred, cred_share, cred_cost = st.sidebar.columns(3)
+    bool_credit = cred.checkbox("Credit Card")
+    percent_credit = cred_share.number_input(
+        "3-Share of Vol. (%):", value=0.0, step=1.0
+    )
+    percent_credit_formatted = "{:,.1%}".format(percent_credit / 100)
+    cost_credit = cred_cost.number_input("3-Assumed Costs:", value=0.0, step=1.0)
+    cost_credit_formatted = "{:,.1%}".format(cost_credit / 100)
+
+    deb, deb_share, deb_cost = st.sidebar.columns(3)
+    bool_debit = deb.checkbox("Direct Debit")
+    percent_debit = deb_share.number_input("4-Share of Vol. (%):", value=50.0, step=1.0)
+    percent_debit_formatted = "{:,.1%}".format(percent_debit / 100)
+    cost_debit = deb_cost.number_input("4-Assumed Costs:", value=0.2, step=1.0)
+    cost_debit_formatted = "{:,.1%}".format(cost_debit / 100)
+
+    pal, pal_share, pal_cost = st.sidebar.columns(3)
+    bool_paypal = pal.checkbox("PayPal")
+    percent_paypal = pal_share.number_input(
+        "5-Share of Vol. (%):", value=30.0, step=1.0
+    )
+    percent_paypal_formatted = "{:,.1%}".format(percent_paypal / 100)
+    cost_paypal = pal_cost.number_input("5-Assumed Costs:", value=1.5, step=1.0)
+    cost_paypal_formatted = "{:,.1%}".format(cost_paypal / 100)
+
+    oher, other_share, other_cost = st.sidebar.columns(3)
+    bool_other = oher.checkbox("Other (please specify)")
+    percent_other = other_share.number_input(
+        "6-Share of Vol. (%):", value=20.0, step=1.0
+    )
+    percent_other_formatted = "{:,.1%}".format(percent_other / 100)
+    cost_other = other_cost.number_input("6-Assumed Costs:", value=0.0, step=1.0)
+    cost_other_formatted = "{:,.1%}".format(cost_other / 100)
+    # check box to show/hide a dataframe
+    payment_df = pd.DataFrame(
+        [
+            {
+                "Current B2B online payment solutions": "Inhouse BNPL",
+                "Yes/ No": bool_inhouse,
+                "Share of total B2B online volume": percent_inhouse_formatted,
+                "Assumed costs": cost_inhouse_formatted,
+            },
+            {
+                "Current B2B online payment solutions": "External BNPL",
+                "Yes/ No": bool_ext,
+                "Share of total B2B online volume": percent_ext_formatted,
+                "Assumed costs": cost_ext_formatted,
+            },
+            {
+                "Current B2B online payment solutions": "Credit Card",
+                "Yes/ No": bool_credit,
+                "Share of total B2B online volume": percent_credit_formatted,
+                "Assumed costs": cost_credit_formatted,
+            },
+            {
+                "Current B2B online payment solutions": "Debit Card",
+                "Yes/ No": bool_debit,
+                "Share of total B2B online volume": percent_debit_formatted,
+                "Assumed costs": cost_debit_formatted,
+            },
+            {
+                "Current B2B online payment solutions": "Paypal",
+                "Yes/ No": bool_paypal,
+                "Share of total B2B online volume": percent_paypal_formatted,
+                "Assumed costs": cost_paypal_formatted,
+            },
+            {
+                "Current B2B online payment solutions": "Other",
+                "Yes/ No": bool_other,
+                "Share of total B2B online volume": percent_other_formatted,
+                "Assumed costs": cost_other_formatted,
+            },
+        ]
+    )
+
+    ###-----------
+
+    ###-----------
+    return payment_df
