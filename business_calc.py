bd173bc86390bf5ffdf852a4ec3811a036276a8d
mobi235 <mohamad.bezreh@billie.io>
2023-05-08 17:54:18 +0200

    fix plotly

--- business_calc.py
+++ business_calc.py
@@ -14,7 +14,8 @@ from layout import (
     get_assumptions,
     coalesce,
     style_dataframe,
-)  # , waterfall_fig
+    waterfall_fig,
+)  #
 
 import sys
 
@@ -647,12 +648,12 @@ tab2.dataframe(payment_output_df)
 # tab2.write(credit_card)
 
 
-# tab3.plotly_chart(
-#     waterfall_fig(
-#         revenue=revenue,
-#         revenue_chg_basket_size=revenue_chg_basket_size,
-#         revenue_chg_conversion_rate=revenue_chg_conversion_rate,
-#         revenue_chg_acceptance_rate=revenue_chg_acceptance_rate,
-#         revenue_w_billie=revenue_w_billie,
-#     )
-# )
+tab3.plotly_chart(
+    waterfall_fig(
+        revenue=revenue,
+        revenue_chg_basket_size=revenue_chg_basket_size,
+        revenue_chg_conversion_rate=revenue_chg_conversion_rate,
+        revenue_chg_acceptance_rate=revenue_chg_acceptance_rate,
+        revenue_w_billie=revenue_w_billie,
+    )
+)
